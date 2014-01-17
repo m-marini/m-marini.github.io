4431159aee68005d7cfcf3286e4e9206159af16e
Marco Marini <marco.marini@mmarini.org>
2014-01-17 13:44:15 +0100

    Start up

---
+++ mmarini/WebContent/js/mmarini.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+var mmarini = mmarini || {};
+
+mmarini.init = function() {
+	$("#main-tab").tabs({
+		beforeLoad : function(event, ui) {
+			ui.jqXHR.error(function(s, e, x) {
+				console.log("Error " + e + x);
+				ui.panel.html("Error " + e);
+			});
+		}
+	});
+	//	
+	// $("#projects").accordion({
+	// heightStyle : "fill"
+	// });
+	// $("#downloads").accordion({
+	// heightStyle : "content"
+	// });
+};
